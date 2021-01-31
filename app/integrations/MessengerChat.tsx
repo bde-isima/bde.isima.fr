7c079711eaaec19d5e5f191e9fff936d3fbbdec1
Ira Skyx <adrien.lenoir42440@gmail.com>
2021-01-31 19:25:10 +0100

    Fix messenger not disappearing and zIndex too high

--- app/integrations/MessengerChat.tsx
+++ app/integrations/MessengerChat.tsx
@@ -1,8 +1,13 @@
+import { useEffect } from "react"
 import MessengerCustomerChat from "react-messenger-customer-chat"
 
 export default function MessengerChat() {
+  //On unmount, cleans up the messenger-chat from the DOM
+  useEffect(() => () => document.getElementById("fb-root")?.remove())
+
   return (
     <MessengerCustomerChat
+      style={{ zIndex: 999 }}
       pageId="913191802071318"
       appId="237417597136510"
       htmlRef="fb-msgr"
