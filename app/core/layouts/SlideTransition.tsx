7901770b5c806f23fa24a95628d7f2f8de4954b4
Adrien Lenoir <adrien@kmtx.com>
2022-01-31 14:44:18 +0100

    fix: change children type of Slide transition to comply with mui

--- app/core/layouts/SlideTransition.tsx
+++ app/core/layouts/SlideTransition.tsx
@@ -4,7 +4,7 @@ import { TransitionProps } from '@mui/material/transitions'
 
 export default forwardRef(function Transition(
   props: TransitionProps & {
-    children?: React.ReactElement<any, any>
+    children: React.ReactElement<any, any>
   },
   ref: React.Ref<unknown>
 ) {
