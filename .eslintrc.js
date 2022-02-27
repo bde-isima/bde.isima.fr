d4019175cbb95defcfeee0431491fbb5ee514e37
Lenoir Adrien <planete.skyx@gmail.com>
2022-02-27 19:20:29 +0000

    chore: improve local setup and instructions

--- .eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ['blitz'],
-  rules: {
-    'jsx-a11y/anchor-has-content': 'off',
-    'no-restricted-imports': ['error', { patterns: ['@mui/*/*/*', '!@mui/material/test-utils/*'] }],
-  },
-}
