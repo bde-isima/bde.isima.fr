0a0e1795a2dde51dd6b15596808158a49b6f0e0f
Lenoir Adrien <planete.skyx@gmail.com>
2022-07-02 19:48:08 +0200

    chore: upgrade all dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,11 @@ const colors = {
   primary: '#2A2E43',
   secondary: '#fff',
   black: '#303030',
-  danger: '#C91F37',
+  danger: '#C91F37'
 }
 
 module.exports = {
-  mode: 'jit',
-  purge: ['{app,mail}/**/*.{js,jsx,ts,tsx}'],
-  darkMode: 'media',
+  content: ['app/**/*.{ts,tsx}'],
   important: true,
   theme: {
     extend: {
@@ -17,44 +15,38 @@ module.exports = {
         sm: '600px',
         md: '900px',
         lg: '1200px',
-        xl: '1536px',
+        xl: '1536px'
         // https://mui.com/customization/breakpoints/#default-breakpoints
       },
       transitionProperty: {
-        width: 'width',
+        width: 'width'
       },
       backgroundImage: () => ({
         background: 'url(/static/images/illustrations/Background.svg)',
-        mobileBackground: 'url(/static/images/illustrations/MobileBackground.svg)',
+        mobileBackground: 'url(/static/images/illustrations/MobileBackground.svg)'
       }),
       backgroundColor: (theme) => ({
         ...theme('colors'),
-        ...colors,
+        ...colors
       }),
       textColor: (theme) => ({
         ...theme('colors'),
-        ...colors,
+        ...colors
       }),
       borderColor: (theme) => ({
         ...theme('colors'),
-        ...colors,
+        ...colors
       }),
       minWidth: {
-        '1/2': '50%',
+        '1/2': '50%'
       },
       minHeight: {
         100: '100px',
-        main: 'calc(100vh - 64px)',
+        main: 'calc(100vh - 64px)'
       },
       maxHeight: {
-        card: 345,
-      },
-    },
-  },
-  variants: {
-    extend: {
-      width: ['focus'],
-    },
-  },
-  plugins: [],
+        card: 345
+      }
+    }
+  }
 }
