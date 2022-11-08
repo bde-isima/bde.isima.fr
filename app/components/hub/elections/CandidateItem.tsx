49190bdf3810b85750e6a30a2f2faa3d25f07a18
Venceslas <duet.venceslas@orange.fr>
2022-11-08 22:21:08 +0000

    Removing react-palette reference

--- app/components/hub/elections/CandidateItem.tsx
+++ app/components/hub/elections/CandidateItem.tsx
@@ -5,40 +5,37 @@ import Grid from '@mui/material/Grid';
 import Skeleton from '@mui/material/Skeleton';
 import Typography from '@mui/material/Typography';
 import { Candidate } from 'db';
-import { Palette } from 'react-palette';
+
+import Image from 'next/image';
 
 type CandidateItemProps = {
   candidate?: Partial<Candidate>;
   onSelect?: (open, value) => () => void;
   isLoading?: boolean;
 };
-
+// TODO: Improve design
 export default function CandidateItem({ candidate, onSelect, isLoading }: CandidateItemProps) {
   return (
     <Grid container item xs={12} md={6}>
-      <CardActionArea className="w-full rounded-full my-4" onClick={onSelect && onSelect(true, candidate)}>
-        <Palette src={candidate?.image || ''}>
-          {({ data }) => (
-            <Card className="flex h-28 md:h-52 rounded-full" style={{ backgroundColor: data.lightMuted }}>
-              <div
-                className="w-28 md:w-52 bg-contain bg-no-repeat bg-center relative"
-                style={{ backgroundImage: `url(${candidate?.image})` }}
-              >
-                <div
-                  className="absolute inset-0"
-                  style={{
-                    background: `linear-gradient(to right, ${data.lightMuted} 5%, ${data.lightMuted}00)`
-                  }}
-                />
-              </div>
-              <CardContent className="flex flex-grow items-center">
-                <Typography className="flex flex-grow" variant="h3">
-                  {isLoading ? <Skeleton width="80%" animation="wave" /> : <i>{candidate?.name}</i>}
-                </Typography>
-              </CardContent>
-            </Card>
+      <CardActionArea className="w-full my-4" onClick={onSelect && onSelect(true, candidate)}>
+        <Card className="p-3 flex gap-3">
+          {candidate?.image && (
+            <Image
+              src={candidate?.image}
+              width={100}
+              height={100}
+              alt={`Liste ${candidate?.name}`}
+              style={{
+                flex: '0 0 100px'
+              }}
+            />
           )}
-        </Palette>
+          <CardContent className="flex-1">
+            <Typography variant="h3">
+              {isLoading ? <Skeleton width="80%" animation="wave" /> : <i>{candidate?.name}</i>}
+            </Typography>
+          </CardContent>
+        </Card>
       </CardActionArea>
     </Grid>
   );
