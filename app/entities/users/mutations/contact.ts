5f8ed688ad09b820eb5f6e51cd4818cacec3f550
Venceslas <duet.venceslas@orange.fr>
2023-01-11 10:23:21 +0000

    Fix contact mail issue

--- app/entities/users/mutations/contact.ts
+++ app/entities/users/mutations/contact.ts
@@ -12,7 +12,7 @@ export default resolver.pipe(async ({ subject, message, email }: ContactInput) =
   try {
     await mail.send({
       subject,
-      to: `${process.env.NODE_ENV !== 'production' ? 'bde.isima' : 'bde.isima.webmaster'}+contact@gmail.com`,
+      to: `${process.env.CONTACT_MAIL}`,
       view: 'contact',
       variables: {
         subject,
