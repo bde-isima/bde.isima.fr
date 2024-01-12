c84fa778d7e0d12ef6d4dbe70035a5536794a2fe
Venceslas <contact@citorva.fr>
2024-01-12 22:57:06 +0000

    hiding troll and listeux technical clubs from non-priviledged users

--- app/entities/clubs/queries/getClubs.ts
+++ app/entities/clubs/queries/getClubs.ts
@@ -4,7 +4,25 @@ import { resolver } from '@blitzjs/rpc';
 
 type GetClubsInput = Pick<Prisma.ClubFindManyArgs, 'where' | 'orderBy' | 'skip' | 'take'>;
 
-export default resolver.pipe(async ({ where, orderBy, skip = 0, take }: GetClubsInput) => {
+export default resolver.pipe(async ({ where, orderBy, skip = 0, take }: GetClubsInput, ctx) => {
+  console.log(ctx.session.roles);
+
+  if (ctx.session.roles?.includes('*') != true && ctx.session.role?.includes('bde') != true) {
+    const limit: Prisma.ClubWhereInput = {
+      NOT: {
+        OR: [{ name: 'listeux' }, { name: 'troll' }]
+      }
+    };
+
+    if (where) {
+      where = {
+        AND: [where, limit]
+      };
+    } else {
+      where = limit;
+    }
+  }
+
   const clubs = await db.club.findMany({
     where,
     orderBy,
